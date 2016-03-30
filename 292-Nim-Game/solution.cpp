20d6ed9209ba63013dd86cf852f7ce27f3654a26
George Chih <chihgeorge@gmail.com>
2016-03-29 22:35:44 -0700

    [292. Nim Game][Accepted]committed by LeetCode Extension

--- 292-Nim-Game/solution.cpp
+++ 292-Nim-Game/solution.cpp
@@ -2,5 +2,13 @@ class Solution {
 public:
     bool canWinNim(int n) {
         return n % 4 != 0;
+        
+        //  Thoughts:
+        //  Since the one who removes the last stone will be the winner, 
+        //  we have to make sure we can remove 1-3 stones in the last turn.
+        //  As we always go first, if the number of stones can be divided by 4, 
+        //  whatever any number of stones we pick, we will always lose the game.
+        //  (We pick 1,2 or 3, and the friends can pick 3, 2 or 1)
+        //  If the number of stones can not be divided by 4, we will always win.
     }
 };
