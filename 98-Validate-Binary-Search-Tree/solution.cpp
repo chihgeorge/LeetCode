8b5ce4f4947ac92bb270d44e0ec1a46c5bd1aa59
George Chih <chihgeorge@gmail.com>
2016-04-04 15:41:05 -0700

    [98. Validate Binary Search Tree][Runtime Error]committed by LeetCode Extension

---
+++ 98-Validate-Binary-Search-Tree/solution.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return valid(root, INT_MIN, INT_MAX);
+    }
+    
+    bool valid(TreeNode *p, int min, int max){
+        if(p == NULL) return true;
+        
+        return  p->val > p->left->val && p->val < p->right->val &&
+                valid(p->left, min, p->val) && valid(p->right, p->val, max);
+    }
+};
