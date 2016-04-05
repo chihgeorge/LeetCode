9ffde21caffcdcc4c2f2cf181fcf0b73cd52beee
George Chih <chihgeorge@gmail.com>
2016-04-04 22:46:06 -0700

    [98. Validate Binary Search Tree][Wrong Answer]committed by LeetCode Extension

--- 98-Validate-Binary-Search-Tree/solution.cpp
+++ 98-Validate-Binary-Search-Tree/solution.cpp
@@ -16,7 +16,7 @@ public:
     bool valid(TreeNode *p, long low, long high){
         if(p == NULL) return true;
         
-        return  (low == NULL || p->val > low) && (p->val < high || high == NULL) 
+        return  (low || p->val > low) && (p->val < high || high) 
                 && valid(p->left, low, p->val) 
                 && valid(p->right, p->val, high);
     }
