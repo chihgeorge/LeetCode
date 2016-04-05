ead182fda228d05fdfbbdbf45fe855633370fe09
George Chih <chihgeorge@gmail.com>
2016-04-05 16:38:14 -0700

    [110. Balanced Binary Tree][Wrong Answer]committed by LeetCode Extension

---
+++ 110-Balanced-Binary-Tree/solution.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        
+    }
+    
+    int maxDepth(TreeNode *root){
+        if(!root)   return 0;
+        int L = maxDepth(root->left);
+        if(L == -1) return -1;
+        int R = maxDepth(root->right);
+        if(R == -1) return -1;
+        
+        return (abs(L - R) > 1) ? -1 : max(L,R) + 1;
+    }
+};
