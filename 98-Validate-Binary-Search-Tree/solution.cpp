19073e094049d88ce6e2279ba43d895f7583d15a
George Chih <chihgeorge@gmail.com>
2016-04-04 23:51:47 -0700

    [98. Validate Binary Search Tree][Wrong Answer]committed by LeetCode Extension

--- 98-Validate-Binary-Search-Tree/solution.cpp
+++ 98-Validate-Binary-Search-Tree/solution.cpp
@@ -9,13 +9,28 @@
  */
 class Solution {
 public:
+    TreeNode *prep = NULL;
     bool isValidBST(TreeNode* root) {
-        return valid(root, NULL, NULL);
+        //return valid(root, NULL, NULL);
+        return isIncreasing(root);
     }
     
-    bool valid(TreeNode *p, TreeNode *low, TreeNode *high){
+    /*bool valid(TreeNode *p, TreeNode *low, TreeNode *high){
         if(p == NULL) return true;
         if(low && p->val <= low->val || high && p->val >= high->val) return false;
         return  valid(p->left, low, p) && valid(p->right, p, high);
+    }*/
+    
+    bool isIncreasing(TreeNode *p){
+        if( p == NULL)  return true;
+        if(isIncreasing(p->left)){
+            if(prep != NULL && prep->val > p->val)  return false;
+            prep = p;
+            
+            return isIncreasing(p->right);
+        }
+        
+        return false;
     }
+    
 };
