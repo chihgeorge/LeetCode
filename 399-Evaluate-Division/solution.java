3d8bbc408b50f67d2fc10cda82b2f39e2bebdfef
George Chih <chihgeorge@gmail.com>
2016-10-03 22:54:36 -0700

    [399. Evaluate Division][Accepted]committed by LeetCode Extension

--- 399-Evaluate-Division/solution.java
+++ 399-Evaluate-Division/solution.java
@@ -1,26 +1,26 @@
 public class Solution {
     public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
         Map<String, Map<String, Double>> numberMap = new HashMap();
-        int i = 0;
-        for(String[] pair: equations){
-            addPairs(numberMap, pair[0], pair[1], values[i]);
-            addPairs(numberMap, pair[1], pair[0], 1.0/values[i]);
+        for(int i = 0; i < equations.length; i++){
+            addPairs(numberMap, equations[i][0], equations[i][1], values[i]);
+            addPairs(numberMap, equations[i][1], equations[i][0], 1.0/values[i]);
         }
         
         double[] result = new double[queries.length];
-        i = 0;
         
-        for(String[] pair: queries){
-            double res = findPairs(numberMap, pair[0], pair[1], new HashSet<>());
-            if(res == null){
-                result[i++] = -1.0;
+        for(int i = 0; i < queries.length; i++){
+            Double res = findPairs(numberMap, queries[i][0], queries[i][1], new HashSet<>());
+            if(res != null){
+                result[i] = res;
             }else{
-                result[i++] = res;
+                result[i] = -1.0;
             }
         }
+        
+        return result;
     }
     
-    private void addPairs(Map<String, Map<String, Double>> numberMap, String num1, String num2, double value){
+    private void addPairs(Map<String, Map<String, Double>> numberMap, String num1, String num2, Double value){
         Map<String, Double> pairMap = numberMap.get(num1);
         if(pairMap == null) {
             pairMap = new HashMap<>();
@@ -29,20 +29,20 @@ public class Solution {
         pairMap.put(num2, value);
     }
     
-    private double findPairs(Map<String, Map<String, Double>> numberMap, String num1, String num2, Set<String> visited){
+    private Double findPairs(Map<String, Map<String, Double>> numberMap, String num1, String num2, Set<String> visited){
         String pairKey = num1 + "," + num2;
         if(visited.contains(pairKey)){
             return null;
         }else if(!numberMap.containsKey(num1) || (!numberMap.containsKey(num2))){
             return null;
-        }else if(num1 == num2){
+        }else if(num1.equals(num2)){
             return 1.0;
         }
         
-        Map<String, Double> pairMap = numMap.get(num1);
+        Map<String, Double> pairMap = numberMap.get(num1);
         visited.add(pairKey);
         for(String key : pairMap.keySet()) {
-            double result = findPairs(numberMap, key, num2, visited);
+            Double result = findPairs(numberMap, key, num2, visited);
             if(result != null) {
                 return result * pairMap.get(key);
             }
