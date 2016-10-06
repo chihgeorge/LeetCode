c03a353f2df793ed9bd3666d68b5b19bce65a671
George Chih <chihgeorge@gmail.com>
2016-10-05 22:01:16 -0700

    [332. Reconstruct Itinerary][Compile Error]committed by LeetCode Extension

---
+++ 332-Reconstruct-Itinerary/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<String> findItinerary(String[][] tickets) {
+        Map<String, PriorityQueue<String>> map = new HashMap<>();
+        for (String[] ticket : tickets) {
+            map.add(ticket[0], new PriorityQueue()).add(ticket[1]);
+        }
+    
+        List<String> result = new LinkedList();
+        Stack<String> stack = new Stack<>();
+
+        stack.push("JFK");
+
+        while (!stack.empty()) {
+	        while (map.containsKey(stack.peek()) && !map.get(stack.peek()).isEmpty()) {
+                String currentStop = stack.peek();
+		        String nextStop = map.get(currentStop).poll();
+		        stack.push(nextStop);
+	        }
+            result.add(0, stack.pop());
+        }
+        return result; 
+    }
+}
